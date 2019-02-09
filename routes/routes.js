9b8e2141e57dcbea5cce87e914b9877344d6b3da
yashraikar1087 <yashraikar1087@gmail.com>
2019-02-09 10:01:05 -0700

    added csv logic

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,20 @@
+var fs = require("fs");
+
 var appRouter = function(app) {
   app.get("/", function(req, res) {
     res.status(200).send("Welcome to our JSON-CSV parser API");
   });
 
   app.post("/csv", function(req, res) {
+    fs.writeFile("./procurement.csv", "test", "utf8", function(err) {
+      if (err) {
+        console.log(
+          "Some error occured - file either not saved or corrupted file saved."
+        );
+      } else {
+        console.log("It's saved!");
+      }
+    });
     console.log(req.body);
     res.send(req.body);
   });
