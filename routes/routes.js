35f88ad5091f2d931e56f0644ac02881648bda04
yashraikar1087 <yashraikar1087@gmail.com>
2019-02-09 09:01:10 -0700

    csv route added

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,11 @@
 var appRouter = function(app) {
   app.get("/", function(req, res) {
-    res.status(200).send("Welcome to our restful API");
+    res.status(200).send("Welcome to our JSON-CSV parser API");
+  });
+
+  app.post("/csv", function(req, res) {
+    console.log(req.body);
+    res.send(req.body);
   });
 };
 
